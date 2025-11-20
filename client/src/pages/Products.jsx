45c60ed61b546ea90a30942ce48ec49dac498590
Brian Cheptiony <cheptiony6@gmail.com>
2025-11-20 23:44:10 +0300

    home

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -3,7 +3,7 @@ import { products } from "../data/products";
 
 export default function Products() {
   return (
-    <div className="p-6 grid md:grid-cols-3 gap-6">
+    <div className="p-6 grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {products.map((p) => (
         <ProductCard key={p.id} product={p} onOpenDetail={() => {}} />
       ))}
