6944a117bc08999eca88adf2fc49dd2151fb5e95
Brian Cheptiony <cheptiony6@gmail.com>
2026-01-27 16:08:12 +0300

    update products

--- client/src/data/products.js
+++ client/src/data/products.js
@@ -18,20 +18,20 @@ export const products = [
     description: "Anti-acne serum from castor with vitamin E."
   },
   {
+    id: 5,
+    name: "Hair Mosturizer",
+    price: 550,
+    size: "150ml",
+    image: cleanser,
+    description: "For hair relaxation and moisturization."
+  },
+  {
     id: 6,
     name: "Anti-Blemish Oil",
     price: 1100,
     size: "200ml",
     image: blemish,
     description: "Fades marks and evens tone with castor."
-  },
-  {
-    id: 7,
-    name: "Body Butter",
-    price: 1050,
-    size: "170g",
-    image: butter,
-    description: "Ultra hydration blend with castor."
   },
   {
     id: 8,
@@ -42,28 +42,20 @@ export const products = [
     description: "Repairs cracked heels quickly."
   },
   {
-    id: 5,
-    name: "Hair Mosturizer",
-    price: 550,
-    size: "150ml",
-    image: cleanser,
-    description: "For hair relaxation and moisturization."
-  },
-  {
-    id: 9,
-    name: "Acne Control Soap",
-    price: 250,
-    size: "80g",
-    image: soap,
-    description: "Cleans acne-prone skin gently."
-  },
-  {
     id: 1,
     name: "Pure Cold-Pressed Oil",
     price: 650,
     size: "100ml",
     image: oil1,
     description: "Hydrates and repairs dry and damaged skin."
+  },
+  {
+    id: 7,
+    name: "Body Butter",
+    price: 1050,
+    size: "170g",
+    image: butter,
+    description: "Ultra hydration blend with castor."
   },
   {
     id: 3,
@@ -81,5 +73,12 @@ export const products = [
     image: cream,
     description: "Full body and hair moisturizer from castor oil."
   },
-
+  {
+    id: 9,
+    name: "Acne Control Soap",
+    price: 250,
+    size: "80g",
+    image: soap,
+    description: "Cleans acne-prone skin gently."
+  },
 ];
